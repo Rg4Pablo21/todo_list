3fa156056d80b167e28bb6733b3e88031c44837f
RG4Pablo21 <gic438@gmail.com>
2025-08-07 13:53:20 -0600

    Se termina de trabajar la adaptación de telefono

--- components/informacion/informacionComponent.js
+++ components/informacion/informacionComponent.js
@@ -1,18 +1,22 @@
+import { formularios } from "../formulario/formularioComponent.js";
+
 export function informaciones(tarea) {
-    // Contenedor general
     let contenedor = document.createElement('div');
     contenedor.className = "contenedor-informacion";
 
-    // ---------------- BOTONES SUPERIORES ----------------
     let divBotones = document.createElement('div');
     divBotones.className = "div-botones";
 
-    // Botón + tarea
     let btnTarea = document.createElement('button');
     btnTarea.className = "btn-tarea";
     btnTarea.innerText = "+ tarea";
 
-    // Botón archivados
+    btnTarea.addEventListener("click", () => {
+        document.body.appendChild(formularios(() => {
+            console.log("Formulario cerrado.");
+        }));
+    });
+
     let btnArchivados = document.createElement('button');
     btnArchivados.className = "btn-archivados";
     btnArchivados.innerText = "Archivados";
@@ -20,35 +24,29 @@ export function informaciones(tarea) {
     divBotones.appendChild(btnTarea);
     divBotones.appendChild(btnArchivados);
 
-    // ---------------- TARJETA DE INFORMACIÓN ----------------
     let tarjeta = document.createElement('div');
     tarjeta.className = "div-informacion";
 
-    // Estado
     let divEstado = document.createElement('div');
     divEstado.className = "estado-tarea";
     divEstado.innerText = tarea.estado;
     tarjeta.appendChild(divEstado);
 
-    // Título
     let titulo = document.createElement('h3');
     titulo.className = "titulo-asignacion";
     titulo.innerText = tarea.titulo;
     tarjeta.appendChild(titulo);
 
-    // Descripción
     let descripcion = document.createElement('p');
     descripcion.className = "descripcion-asignacion";
     descripcion.innerText = tarea.descripcion;
     tarjeta.appendChild(descripcion);
 
-    // Integrantes
     let spanIntegrantes = document.createElement('span');
     spanIntegrantes.className = "span-integrantes";
     spanIntegrantes.innerText = "Integrantes";
     tarjeta.appendChild(spanIntegrantes);
 
-    // Lista de integrantes
     let divIntegrantes = document.createElement('div');
     divIntegrantes.className = "div-integrantes";
     tarea.integrantes.forEach(icono => {
@@ -59,7 +57,6 @@ export function informaciones(tarea) {
     });
     tarjeta.appendChild(divIntegrantes);
 
-    // Añadimos todo al contenedor principal
     contenedor.appendChild(divBotones);
     contenedor.appendChild(tarjeta);
 
